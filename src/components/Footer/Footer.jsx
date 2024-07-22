c38d1186b67bb07d3d16a53448b827585a67ba71
andreasgiannoudis <agiannoudis@hotmail.com>
2024-07-22 18:39:10 +0200

    resolved the issue with the image in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaLinkedin, FaGithub, FaFileAlt } from "react-icons/fa";
 import cv from '../../assets/cv/cv_en.pdf';
+import backgroundImage from '../../assets/img/dark3.jpg';
 
 function Footer() {
   const today = new Date();
   const year = today.getFullYear();
 
   return (
-    <footer className="footer">
+    <footer className="footer" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="overlay">
         <div className="social-icons">
           <a
