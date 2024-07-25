65c8809f332ef3e698213153372d67e187a00827
andreasgiannoudis <agiannoudis@hotmail.com>
2024-07-25 15:37:55 +0200

    Added my newest project

--- src/components/Projects/projectdata.js
+++ src/components/Projects/projectdata.js
@@ -11,35 +11,44 @@ import project1 from '/src/assets/img/project-images/portfolio.png';
 import project2 from '/src/assets/img/project-images/wordpress-project-lorum.png';
 import project3 from '/src/assets/img/project-images/webbshopp.png';
 import project4 from '/src/assets/img/project-images/productpage.png';
+import project5 from '/src/assets/img/project-images/movies-series.png';
 const projectData = [
     {
       id: 1,
+      title: 'Popular movies and series',
+      description: 'Movies/series webapp created with Next.js and Scss. Designed and developed by me. Search movies and series. Discover the popular movies and series by navigation to each page.',
+      image: project5,
+      link: 'https://github.com/andreasgiannoudis/popular-movies-series'
+    },
+    {
+      id: 2,
       title: 'Portfolio',
       description: 'The portfolio you are seeing right now. Built with React and SASS for styling. Designed and built by me!',
       image: project1,
       link: '/'
     },
     {
-      id: 2,
+      id: 3,
       title: 'Wordpress Project',
       description: 'Wordpress project that is completely custom, own theme, used by php and wordpress editor and sass for styling. All content is dynamic. Unfortunately it is not live so you can see it',
       image: project2,
       link: '#'
     },
     {
-      id: 3,
+      id: 4,
       title: 'Web Shop',
       description: 'Web shop using React and Node.js. You can add products to the cart and purchase them (no real payment).',
       image: project3,
       link: 'https://github.com/andreasgiannoudis/webb23-js2-slutprojekt-andreas-giannoudis'
     },
     {
-      id: 4,
+      id: 5,
       title: 'Wordpress product page',
       description: 'Product page created with wordpress/woocommerce. Use PHP, Sass and Vitejs. Everything is customized except the product, is created in woocommerce. Unfortunately they are not live.',
       image: project4,
       link: '#'
     },
+    
   ];
   
   export default projectData;
